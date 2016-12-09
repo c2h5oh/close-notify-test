e387122d831aed50988f88c3579d0e259a5f92eb
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2016-12-09 13:09:53 -0500

    Another try

--- test.go
+++ test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/middleware"
@@ -10,12 +11,10 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 	r.Use(middleware.CloseNotify)
-
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(100 * time.Millisecond)
 		v := map[string]interface{}{"x": 1}
-
 		render.Status(r, http.StatusBadRequest)
 		render.JSON(w, r, v)
 	})
